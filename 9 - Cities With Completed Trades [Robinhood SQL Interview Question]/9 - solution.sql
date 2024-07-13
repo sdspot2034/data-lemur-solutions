3624010109938ca0658804dcc729eb2b580db670
sdspot2034 <shreyandas2034@gmail.com>
2024-07-13 18:47:36 +0530

    Solved Problem 9

--- 9 - Cities With Completed Trades [Robinhood SQL Interview Question]/9 - solution.sql
+++ 9 - Cities With Completed Trades [Robinhood SQL Interview Question]/9 - solution.sql
@@ -1,1 +1,10 @@
 -- Solved on: PostgreSQL 14
+SELECT 
+  city
+  , COUNT(DISTINCT order_id) AS total_orders
+FROM trades A
+LEFT JOIN users B ON A.user_id = B.user_id 
+WHERE status = 'Completed'
+GROUP BY city
+ORDER BY 2 DESC
+LIMIT 3;
