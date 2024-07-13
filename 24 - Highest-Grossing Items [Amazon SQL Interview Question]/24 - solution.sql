3e55b0cf7e91bffd27e91ccad0dc826d31a94967
sdspot2034 <shreyandas2034@gmail.com>
2024-07-13 19:02:31 +0530

    Solved Problem 24

--- 24 - Highest-Grossing Items [Amazon SQL Interview Question]/24 - solution.sql
+++ 24 - Highest-Grossing Items [Amazon SQL Interview Question]/24 - solution.sql
@@ -1,1 +1,19 @@
 -- Solved on: PostgreSQL 14
+WITH cte_product_wise_spend AS (
+  SELECT 
+    category
+    , product
+    , SUM (spend) total_spend
+  FROM product_spend
+  WHERE DATE_PART('year',transaction_date) = 2022
+  GROUP BY category, product
+)
+
+, cte_ranked_listing AS (
+  SELECT *
+  , ROW_NUMBER () OVER (PARTITION BY category ORDER BY total_spend DESC) rnk
+  FROM cte_product_wise_spend
+)
+
+SELECT category, product, total_spend
+FROM cte_ranked_listing WHERE rnk <= 2;
