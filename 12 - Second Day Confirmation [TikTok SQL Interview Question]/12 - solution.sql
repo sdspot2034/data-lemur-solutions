8a07bb90f07896fc6ef30a98dad426dbc84d02e5
sdspot2034 <shreyandas2034@gmail.com>
2024-07-13 18:54:49 +0530

    Solved Problem 12

--- 12 - Second Day Confirmation [TikTok SQL Interview Question]/12 - solution.sql
+++ 12 - Second Day Confirmation [TikTok SQL Interview Question]/12 - solution.sql
@@ -1,1 +1,6 @@
 -- Solved on: PostgreSQL 14
+SELECT user_id FROM emails A
+LEFT JOIN (
+  SELECT * FROM texts WHERE signup_action = 'Confirmed'
+) B on A.email_id = B.email_id
+WHERE DATE_PART('day',B.action_date - A.signup_date) = 1;
