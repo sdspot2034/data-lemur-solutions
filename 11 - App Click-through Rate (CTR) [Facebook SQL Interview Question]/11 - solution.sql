0988da1d86ba7a82c733fba04a641cf127e6aa65
sdspot2034 <shreyandas2034@gmail.com>
2024-07-13 18:51:56 +0530

    Solved Problem 11

--- 11 - App Click-through Rate (CTR) [Facebook SQL Interview Question]/11 - solution.sql
+++ 11 - App Click-through Rate (CTR) [Facebook SQL Interview Question]/11 - solution.sql
@@ -1,1 +1,10 @@
 -- Solved on: PostgreSQL 14
+SELECT 
+  app_id
+  , ROUND(
+      (COUNT(CASE WHEN event_type = 'click' THEN event_type END)) * 100.0
+      / COUNT(CASE WHEN event_type = 'impression' THEN event_type END)
+    , 2) AS ctr
+FROM events
+WHERE DATE_PART('year',timestamp) = 2022
+GROUP BY app_id;
