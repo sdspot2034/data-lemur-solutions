612b1b55fa2341dc64cc513c1ceacf97a2d4d673
sdspot2034 <shreyandas2034@gmail.com>
2024-07-13 18:57:26 +0530

    Solved Problem 13

--- 13 - IBM db2 Product Analytics [IBM SQL Interview Question]/13 - solution.sql
+++ 13 - IBM db2 Product Analytics [IBM SQL Interview Question]/13 - solution.sql
@@ -1,1 +1,17 @@
 -- Solved on: PostgreSQL 14
+SELECT
+  query_counts AS unique_queries
+  , COUNT(employee_id)
+FROM (
+  SELECT 
+    A.employee_id
+    , COUNT(DISTINCT query_id) query_counts
+  FROM employees A
+  LEFT JOIN queries B 
+    ON A.employee_id = B.employee_id
+    AND DATE_PART('year', query_starttime) = '2023'
+    AND DATE_PART('month', query_starttime) BETWEEN 7 AND 9
+  GROUP BY A.employee_id
+) user_query_counts
+GROUP BY query_counts
+ORDER BY 1
