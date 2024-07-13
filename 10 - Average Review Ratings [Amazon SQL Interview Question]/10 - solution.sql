057217b038134bfd125e8f101cd3828416d3661f
sdspot2034 <shreyandas2034@gmail.com>
2024-07-13 18:49:53 +0530

    Solved Problem 10

--- 10 - Average Review Ratings [Amazon SQL Interview Question]/10 - solution.sql
+++ 10 - Average Review Ratings [Amazon SQL Interview Question]/10 - solution.sql
@@ -1,1 +1,8 @@
 -- Solved on: PostgreSQL 14
+SELECT 
+  DATE_PART('month', submit_date) mth
+  , product_id
+  , ROUND(AVG(stars), 2)
+FROM reviews
+GROUP BY product_id, DATE_PART('month', submit_date)
+ORDER BY 1,2;
