e7cd54025983b5503f4229524db60444cbf76b68
sdspot2034 <shreyandas2034@gmail.com>
2024-07-18 00:00:08 +0530

    Solved Problem 18

--- 18 - Pharmacy Analytics (Part 3) [CVS Health SQL Interview Question]/18 - solution.sql
+++ 18 - Pharmacy Analytics (Part 3) [CVS Health SQL Interview Question]/18 - solution.sql
@@ -1,1 +1,7 @@
 -- Solved on: PostgreSQL 14
+SELECT 
+  manufacturer
+  , CONCAT('$',ROUND(SUM(total_sales) / 1000000, 0), ' million')
+FROM pharmacy_sales
+GROUP BY manufacturer
+ORDER BY SUM(total_sales) DESC, manufacturer ASC;
