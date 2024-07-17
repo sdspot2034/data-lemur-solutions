9b35a1488874542ad48bd5433a36079ab99443b9
sdspot2034 <shreyandas2034@gmail.com>
2024-07-17 23:41:05 +0530

    Solved Problem 15

--- 15 - Compressed Mean [Alibaba SQL Interview Question]/15 - solution.sql
+++ 15 - Compressed Mean [Alibaba SQL Interview Question]/15 - solution.sql
@@ -1,1 +1,8 @@
 -- Solved on: PostgreSQL 14
+SELECT 
+  ROUND((sum(total_qty)::numeric / sum(order_occurrences)), 1) as mean
+FROM 
+(SELECT 
+  order_occurrences
+  , item_count * order_occurrences as total_qty
+FROM items_per_order) quantified_orders;
