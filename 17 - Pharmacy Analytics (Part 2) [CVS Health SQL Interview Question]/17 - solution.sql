360773503d713573e2a974a4fd15d483604d92fa
sdspot2034 <shreyandas2034@gmail.com>
2024-07-17 23:53:29 +0530

    Solved Problem 17

--- 17 - Pharmacy Analytics (Part 2) [CVS Health SQL Interview Question]/17 - solution.sql
+++ 17 - Pharmacy Analytics (Part 2) [CVS Health SQL Interview Question]/17 - solution.sql
@@ -1,1 +1,15 @@
 -- Solved on: PostgreSQL 14
+WITH cte_drugs_with_losses AS (
+  SELECT 
+    *, 1 as drug_count, cogs - total_sales losses
+  FROM pharmacy_sales
+  WHERE cogs - total_sales > 0
+)
+
+SELECT
+  manufacturer
+  , SUM(drug_count) AS drug_count
+  , SUM(losses) AS total_loss
+FROM cte_drugs_with_losses
+GROUP BY manufacturer
+ORDER BY 3 DESC;
