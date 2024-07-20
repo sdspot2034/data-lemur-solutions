f7b3e80ad4c3802131a3cb888fc571c1e4c0c128
sdspot2034 <shreyandas2034@gmail.com>
2024-07-20 18:35:00 +0530

    Solved Problem 29

--- 29 - Odd and Even Measurements [Google SQL Interview Question]/29 - solution.sql
+++ 29 - Odd and Even Measurements [Google SQL Interview Question]/29 - solution.sql
@@ -1,1 +1,18 @@
 -- Solved on: PostgreSQL 14
+WITH cte_numbered_measurements AS (
+  SELECT 
+    CAST(measurement_time AS date) AS date
+    , ROW_NUMBER() OVER (
+      PARTITION BY CAST(measurement_time AS date)
+      ORDER BY measurement_time
+    ) AS measurement_num
+    , measurement_value
+  FROM measurements
+)
+
+SELECT date
+  , SUM(CASE WHEN measurement_num % 2 <> 0 THEN measurement_value ELSE 0 END) odd_sum
+  , SUM(CASE WHEN measurement_num % 2 = 0 THEN measurement_value ELSE 0 END) even_sum
+FROM cte_numbered_measurements
+GROUP BY date
+ORDER BY 1;
