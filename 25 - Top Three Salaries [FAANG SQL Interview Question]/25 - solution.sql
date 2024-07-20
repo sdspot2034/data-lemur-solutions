709f2c9d46034a9c98e66802157704a9aac3485b
sdspot2034 <shreyandas2034@gmail.com>
2024-07-20 17:36:34 +0530

    Solved Problem 25

--- 25 - Top Three Salaries [FAANG SQL Interview Question]/25 - solution.sql
+++ 25 - Top Three Salaries [FAANG SQL Interview Question]/25 - solution.sql
@@ -1,1 +1,17 @@
 -- Solved on: PostgreSQL 14
+SELECT 
+  department_name, name, salary
+FROM (
+  SELECT 
+    B.department_name
+    , A.name
+    , A.salary
+    , DENSE_RANK() OVER (
+      PARTITION BY department_name 
+      ORDER BY salary DESC
+    ) AS salary_rank
+  FROM employee A
+  LEFT JOIN department B ON A.department_id = B.department_id
+) ranked_salaries
+WHERE salary_rank <= 3
+ORDER BY department_name, salary DESC, name;
