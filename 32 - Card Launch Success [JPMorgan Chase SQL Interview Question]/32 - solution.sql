410ca7b9c67d1b4461bb068383f9c48e464ac5c5
sdspot2034 <shreyandas2034@gmail.com>
2024-07-20 21:46:48 +0530

    Solved Problem 32

--- 32 - Card Launch Success [JPMorgan Chase SQL Interview Question]/32 - solution.sql
+++ 32 - Card Launch Success [JPMorgan Chase SQL Interview Question]/32 - solution.sql
@@ -1,1 +1,17 @@
 -- Solved on: PostgreSQL 14
+WITH card_launch AS (
+  SELECT 
+    card_name,
+    issued_amount,
+    MAKE_DATE(issue_year, issue_month, 1) AS issue_date,
+    MIN(MAKE_DATE(issue_year, issue_month, 1)) OVER (
+      PARTITION BY card_name) AS launch_date
+  FROM monthly_cards_issued
+)
+
+SELECT 
+  card_name, 
+  issued_amount
+FROM card_launch
+WHERE issue_date = launch_date
+ORDER BY issued_amount DESC;
