8d5ecc76c97b70f9f09dea0b852529218c7a8793
sdspot2034 <shreyandas2034@gmail.com>
2024-07-20 21:57:19 +0530

    Solved Problem 33

--- 33 - International Call Percentage [Verizon SQL Interview Question]/33 - solution.sql
+++ 33 - International Call Percentage [Verizon SQL Interview Question]/33 - solution.sql
@@ -1,1 +1,11 @@
 -- Solved on: PostgreSQL 14
+SELECT 
+  ROUND(
+    COUNT(
+      CASE WHEN CALLER.country_id <> RECEIVER.country_id 
+      THEN A.caller_id END
+    ) * 100.0 / COUNT(A.caller_id)
+  , 1) international_calls_pct
+FROM phone_calls A
+LEFT JOIN phone_info CALLER ON CALLER.caller_id = A.caller_id
+LEFT JOIN phone_info RECEIVER ON RECEIVER.caller_id = A.receiver_id;
