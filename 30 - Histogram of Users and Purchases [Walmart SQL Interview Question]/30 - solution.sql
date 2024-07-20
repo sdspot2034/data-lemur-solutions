75ba4f93a026b91de229c6748b4437f238c6ff6d
sdspot2034 <shreyandas2034@gmail.com>
2024-07-20 21:33:09 +0530

    Solved Problem 30

--- 30 - Histogram of Users and Purchases [Walmart SQL Interview Question]/30 - solution.sql
+++ 30 - Histogram of Users and Purchases [Walmart SQL Interview Question]/30 - solution.sql
@@ -1,1 +1,18 @@
 -- Solved on: PostgreSQL 14
+WITH cte_user_transactions_ranked AS (
+  SELECT 
+    *
+    , DENSE_RANK() OVER (
+        PARTITION BY user_id 
+        ORDER BY transaction_date DESC
+      ) transaction_rank
+  FROM user_transactions
+)
+
+SELECT 
+  transaction_date
+  , user_id
+  , COUNT(product_id)
+FROM cte_user_transactions_ranked 
+WHERE transaction_rank = 1
+GROUP BY transaction_date, user_id;
