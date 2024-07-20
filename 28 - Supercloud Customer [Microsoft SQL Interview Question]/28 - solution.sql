cc36282a5d3aece84c75b872bd34b648e559613a
sdspot2034 <shreyandas2034@gmail.com>
2024-07-20 18:22:58 +0530

    Solved Problem 28

--- 28 - Supercloud Customer [Microsoft SQL Interview Question]/28 - solution.sql
+++ 28 - Supercloud Customer [Microsoft SQL Interview Question]/28 - solution.sql
@@ -1,1 +1,10 @@
 -- Solved on: PostgreSQL 14
+SELECT 
+  A.customer_id
+FROM customer_contracts A
+LEFT JOIN products B ON A.product_id = B.product_id
+GROUP BY customer_id
+HAVING 
+  COUNT(distinct B.product_category) = (
+    SELECT COUNT(DISTINCT product_category) FROM products
+);
