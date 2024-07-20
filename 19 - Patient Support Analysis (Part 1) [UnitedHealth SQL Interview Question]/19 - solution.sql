84b2a6dacee4a6cea9ad811f389b96d00b9c070f
sdspot2034 <shreyandas2034@gmail.com>
2024-07-20 17:24:44 +0530

    Solved Problem 19

--- 19 - Patient Support Analysis (Part 1) [UnitedHealth SQL Interview Question]/19 - solution.sql
+++ 19 - Patient Support Analysis (Part 1) [UnitedHealth SQL Interview Question]/19 - solution.sql
@@ -1,1 +1,7 @@
 -- Solved on: PostgreSQL 14
+SELECT count(policy_holder_id) policy_holder_count FROM (
+  SELECT policy_holder_id
+  FROM callers
+  GROUP BY policy_holder_id
+  HAVING count(distinct case_id) >= 3
+) three_times_callers;
