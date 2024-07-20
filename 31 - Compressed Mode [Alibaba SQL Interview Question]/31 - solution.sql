1050c368f9162f59eed84aea6abc1053ed606b95
sdspot2034 <shreyandas2034@gmail.com>
2024-07-20 21:41:43 +0530

    Solved Problem 31

--- 31 - Compressed Mode [Alibaba SQL Interview Question]/31 - solution.sql
+++ 31 - Compressed Mode [Alibaba SQL Interview Question]/31 - solution.sql
@@ -1,1 +1,12 @@
 -- Solved on: PostgreSQL 14
+WITH cte_orders_ranked AS (
+  SELECT 
+    *
+    , DENSE_RANK() OVER (ORDER BY order_occurrences DESC) as rnk 
+  FROM items_per_order
+)
+
+SELECT 
+  item_count AS mode
+FROM cte_orders_ranked
+WHERE rnk = 1;
