e83f3287fdbba4fffed8673b4a52956c70dfd026
sdspot2034 <shreyandas2034@gmail.com>
2024-07-20 18:15:16 +0530

    Solved Problem 27

--- 27 - Signup Activation Rate [TikTok SQL Interview Question]/27 - solution.sql
+++ 27 - Signup Activation Rate [TikTok SQL Interview Question]/27 - solution.sql
@@ -1,1 +1,10 @@
 -- Solved on: PostgreSQL 14
+SELECT 
+  -- count(DISTINCT A.user_id) AS total_users_emailed
+  -- , count(DISTINCT B.text_id) AS total_users_confirmed
+  ROUND(count(DISTINCT B.text_id) * 1.0 / count(DISTINCT A.user_id), 2)
+FROM emails A
+LEFT JOIN texts B 
+  ON A.email_id = B.email_id 
+  AND B.signup_action = 'Confirmed'
+;
