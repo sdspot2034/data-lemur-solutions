68feeaf2043929b78dde9dd23e5433586c2e0d34
sdspot2034 <shreyandas2034@gmail.com>
2024-07-21 21:29:50 +0530

    Solved Problems 47.

--- 47 - Well Paid Employees [FAANG SQL Interview Question]/47 - solution.sql
+++ 47 - Well Paid Employees [FAANG SQL Interview Question]/47 - solution.sql
@@ -1,1 +1,7 @@
 -- Solved on: PostgreSQL 14
+SELECT 
+  e.employee_id
+  , e.name as employee_name 
+FROM employee e
+LEFT JOIN employee m on e.manager_id = m.employee_id
+WHERE e.salary > m.salary;
