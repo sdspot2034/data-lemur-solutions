f75c1f620d6fc0ab35d96209ca2efb14bae8af5c
sdspot2034 <shreyandas2034@gmail.com>
2024-07-21 20:51:40 +0530

    Solved Problems 39.

--- 39 - Advertiser Status [Facebook SQL Interview Question]/39 - solution.sql
+++ 39 - Advertiser Status [Facebook SQL Interview Question]/39 - solution.sql
@@ -1,1 +1,11 @@
 -- Solved on: PostgreSQL 14
+SELECT 
+  COALESCE(A.user_id, B.user_id)
+  , CASE WHEN B.paid IS NULL THEN 'CHURN'
+      ELSE CASE WHEN A.status = 'CHURN' THEN 'RESURRECT'
+        WHEN A.user_id IS NULL THEN 'NEW'  
+        ELSE 'EXISTING' END
+    END AS new_status
+FROM advertiser A
+FULL JOIN daily_pay B on A.user_id = B.user_id
+ORDER BY 1;
