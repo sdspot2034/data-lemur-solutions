e291f1f991c2d1189711fda523248c711a040593
sdspot2034 <shreyandas2034@gmail.com>
2024-07-21 21:27:00 +0530

    Solved Problems 42.

--- 42 - Who Made Quota? [Oracle SQL Interview Question]/42 - solution.sql
+++ 42 - Who Made Quota? [Oracle SQL Interview Question]/42 - solution.sql
@@ -1,1 +1,8 @@
 -- Solved on: PostgreSQL 14
+SELECT 
+  A.employee_id
+  , CASE WHEN SUM(deal_size) >= quota THEN 'yes' ELSE 'no' END AS made_quota
+FROM sales_quotas A
+LEFT JOIN deals B ON A.employee_id = B.employee_id
+GROUP BY A.employee_id, quota
+ORDER BY 1;
